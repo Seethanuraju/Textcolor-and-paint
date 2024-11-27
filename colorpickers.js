dcfae4e7403dae4d1d85ffa69f28466e799df1bb
Seethanuraju <113660537+Seethanuraju@users.noreply.github.com>
2024-11-27 16:49:32 +0530

    Add files via upload

---
+++ colorpickers.js
@@ -0,0 +1,39 @@
+//colors for text
+function textcolor1(){
+    let t= document.getElementById("text");
+    t.style.color="red";
+}
+function textcolor2(){
+    let t= document.getElementById("text");
+    t.style.color="green";
+}
+function textcolor3(){
+    let t= document.getElementById("text");
+    t.style.color="black";
+}
+//colors for background
+function bgcolor1(){
+    let t= document.getElementById("text");
+    t.style.backgroundColor="red";
+}
+function bgcolor2(){
+    let t= document.getElementById("text");
+    t.style.backgroundColor="green";
+}
+function bgcolor3(){
+    let t= document.getElementById("text");
+    t.style.backgroundColor="black";
+}
+//font styles for text
+function fontstyle1(){
+    let t= document.getElementById("text");
+    t.style.fontFamily="Colonna MT";
+}
+function fontstyle2(){
+    let t= document.getElementById("text");
+    t.style.fontFamily="Times New Roman";
+}
+function fontstyle3(){
+    let t= document.getElementById("text");
+    t.style.fontFamily="SimSun-ExtB";
+}
